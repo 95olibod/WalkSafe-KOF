493749c4c97f62bc66e6b22d4beb2f95bf017142
Milbredt <55477642+Milbredt@users.noreply.github.com>
2021-10-04 15:47:58 +0200

    Updated timerInput
    everything is working. Needs design

--- components/timerInput.tsx
+++ components/timerInput.tsx
@@ -2,33 +2,32 @@ import { Button, StyleSheet, TextInput, View, Text } from "react-native";
 import React, { useState } from "react";
 import NumericInput from 'react-native-numeric-input'
 
-
-function UpdateHours (hours: number) {
-    setHours(hours)
-    setTotalMinutes(minutes + (hours * 60))
-}
-
-function UpdateMinutes (minutes : number) {
-    setMinutes(minutes)
-    setTotalMinutes(minutes + (hours * 60))
-}
-
-const [minutes, setMinutes] = useState(0);
-const [hours, setHours] = useState(0);
-const [totalMinutes, setTotalMinutes] = useState(0);
-
-
-
 function TimerInput () {
+
+    function UpdateHours (hours: number) {
+        setHours(hours)
+        setTotalMinutes(minutes + (hours * 60))
+    }
+    
+    function UpdateMinutes (minutes : number) {
+        setMinutes(minutes)
+        setTotalMinutes(minutes + (hours * 60))
+    }
+
+    const [minutes, setMinutes] = useState(0);
+    const [hours, setHours] = useState(0);
+    const [totalMinutes, setTotalMinutes] = useState(0);
+    
     return (
         <View style={styles.timerInput}>
             <View style={styles.box}>
                 <Text style={styles.text}>Timmar</Text>
-                <NumericInput type='up-down' value={hours} onChange={(x) => UpdateHours(x)} textColor="white"  />
+                <NumericInput type='up-down' value={hours} minValue={0} maxValue={100} onChange={(x) => UpdateHours(x)} textColor="white"  />
             </View>
+            <Text>{minutes} {hours} {totalMinutes}</Text>
             <View>
                 <Text style={styles.text}>Minuter</Text>
-                <NumericInput type='up-down' value={minutes} onChange={(y) => UpdateMinutes(y)} textColor="white"  />
+                <NumericInput type='up-down' value={minutes} minValue={0} maxValue={59} onChange={(y) => UpdateMinutes(y)} textColor="white"  />
             </View>
         </View>
     )
