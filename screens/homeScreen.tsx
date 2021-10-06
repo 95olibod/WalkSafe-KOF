90b3b52c7471085e858cfa650e9695b202ecf4ee
KSigurd <ksigurd10@gmail.com>
2021-10-06 15:03:53 +0200

    feature/47-fix-device-context

--- screens/homeScreen.tsx
+++ screens/homeScreen.tsx
@@ -10,65 +10,67 @@ import TimerInput from "../components/timerInput";
 type Props = NativeStackScreenProps<RootStackParamList, "Kontakter">;
 
 function HomeScreen({ navigation }: Props) {
-  const { setTimerInputFromUser } = useContext(DeviceContext);
+    const { setTimerInputFromUser } = useContext(DeviceContext);
 
-  const [minutes, setMinutes] = useState(0);
-  const [hours, setHours] = useState(0);
-  const [totalMinutes, setTotalMinutes] = useState(0);
+    const [minutes, setMinutes] = useState(0);
+    const [hours, setHours] = useState(0);
+    const [totalMinutes, setTotalMinutes] = useState(0);
 
-  const updateHours = (hours: number) => {
-    setHours(hours);
-    setTotalMinutes(minutes + hours * 60);
-  };
+    const updateHours = (hours: number) => {
+        setHours(hours);
+        setTotalMinutes(minutes + hours * 60);
+    };
 
-  const updateMinutes = (minutes: number) => {
-    setMinutes(minutes);
-    setTotalMinutes(minutes + hours * 60);
-  };
+    const updateMinutes = (minutes: number) => {
+        setMinutes(minutes);
+        setTotalMinutes(minutes + hours * 60);
+    };
 
-  const handlePress = () => {
-    if (totalMinutes >= 1) {
-      setTimerInputFromUser(totalMinutes * 60);
-      navigation.navigate("Kontakter");
-    } 
-  };
+    const handlePress = () => {
+        if (totalMinutes >= 1) {
+            setTimerInputFromUser(totalMinutes);
+            // setTimerInputFromUser(totalMinutes * 60);
+            navigation.navigate("Kontakter");
+        }
+    };
 
-  return (
-    <View style={styles.container}>
-    {totalMinutes < 1 ?
+    return (
+        <View style={styles.container}>
+            {totalMinutes < 1 ? (
+                <Text style={styles.validationText}>
+                    Timer måste minst ställas in på 1 minut
+                </Text>
+            ) : null}
 
-        <Text style={styles.validationText}>
-             Timer måste minst ställas in på 1 minut
-      </Text>
-      : null
-    }
-         
-      <TimerInput updateHours={updateHours} updateMinutes={updateMinutes} />
+            <TimerInput
+                updateHours={updateHours}
+                updateMinutes={updateMinutes}
+            />
 
-      {totalMinutes > 0 ? (
-        <Button
-          title="GÅ vidare till kontakter"
-          onPress={() => handlePress()}
-        ></Button>
-      ) : null}
+            {totalMinutes > 0 ? (
+                <Button
+                    title="GÅ vidare till kontakter"
+                    onPress={() => handlePress()}
+                ></Button>
+            ) : null}
 
-      <Battery />
-    </View>
-  );
+            <Battery />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    height: "100%",
-    alignItems: "center",
-  },
-  text: {
-    color: "white",
-  },
-  validationText: {
-    color: "#FFF",
-    fontSize: 15,
-  },
+    container: {
+        height: "100%",
+        alignItems: "center",
+    },
+    text: {
+        color: "white",
+    },
+    validationText: {
+        color: "#FFF",
+        fontSize: 15,
+    },
 });
 
 export default HomeScreen;
